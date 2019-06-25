1b4d4431b474eaf6d1a8946391b2dec2312ffd22
Hemanth Boddepalli <hemanth.boddepalli@gmail.com>
2019-06-25 23:51:54 +0530

    Create EvalRPN.java

---
+++ 100DaysOfCode/Day 15/EvalRPN.java
@@ -0,0 +1,37 @@
+/*
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+*/
+public class EvalRPN {
+    public int evalRPN(ArrayList<String> A) {
+        Stack<Integer> stack = new Stack<Integer>();
+        for (String element : A) {
+            if (element.equals("/") 
+                || element.equals("*")
+                || element.equals("+")
+                || element.equals("-")) {
+                
+                int a = stack.pop();
+                int b = stack.pop();
+                int res = 0;
+                if (element.equals("/")) {
+                    res = b/a;
+                }
+                else if (element.equals("*")) {
+                    res = b*a;
+                }
+                else if (element.equals("+")) {
+                    res = b + a;
+                }
+                else {
+                    res = a - b;
+                }
+                stack.push(res);
+            }
+            else {
+                stack.push(Integer.parseInt(element));
+            }
+        }
+        return stack.pop();
+    }
+}
