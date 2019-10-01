f3ca70e1751ff57537f42ba3a2a8836da8148494
Hemanth Boddepalli <hemanth.boddepalli@gmail.com>
2019-10-01 13:06:47 +0530

    Create SortList.java

---
+++ LeetCode/SortList.java
@@ -0,0 +1,44 @@
+//https://leetcode.com/problems/sort-list/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class SortList {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode mid = middleNode(head);
+        ListNode head1 = sortList(head);
+        ListNode head2 = sortList(mid);
+        return merge(head1, head2);
+    }
+    private ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = head;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        return slow;
+    }
+    private ListNode merge(ListNode head1, ListNode head2) {
+        if (head1 == null) return head2;
+        if (head2 == null) return head1;
+        if (head1.val < head2.val) {
+            head1.next = merge(head1.next, head2);
+            return head1;
+        }
+        else {
+            head2.next = merge(head1, head2.next);
+            return head2;
+        }
+    }
+}
